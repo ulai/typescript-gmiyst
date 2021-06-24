924103a70de4ed2b7b21ff071bae56f72d78e45d
ulai <ueli@hotmail.com>
2021-06-24 23:15:42 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,34 @@
-// Import stylesheets
-import './style.css';
+import * as Phaser from 'phaser';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+new Phaser.Game({
+  scene: {
+    create
+  },
+  parent: '#app',
+  dom: {
+    createContainer: true,
+    behindCanvas: false
+  }
+});
+
+function create(this: Phaser.Scene) {
+  /*
+  const image = this.add.image(200, 300, '');
+  this.add.dom(200, 100)
+    .createFromHTML(`<input type="file" />`)
+    .addListener('change')
+    .on('change', (ev) => {
+      const file = ev.target.files[0];
+      const reader = new FileReader();
+      reader.addEventListener('load', (event) => {
+        this.textures
+          .addBase64('new-image', event.target.result)
+          .once(Phaser.Textures.Events.LOAD, () => {
+            image.setTexture('new-image');
+          });
+      });
+      reader.readAsDataURL(file);
+    });
+    */
+  this.add.rectangle(10, 10, 20, 20, 0xaabbcc);
+}
